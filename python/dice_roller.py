2ec40c9d4dcfaaa11a7386f8117a9394ecd7f69c
cmillar70 <55702245+cmillar70@users.noreply.github.com>
2021-09-14 10:11:51 -0400

    Update dice_roller.py

--- python/dice_roller.py
+++ python/dice_roller.py
@@ -1,1 +1,12 @@
-
+# Automatic Dice Roller
+# Written by Christian
+input("press enter to roll")
+from random import randint
+print ("Automatic Dice Roller")
+print ("The value is:")
+print (randint(1,6))
+rol = input("roll again?")
+while rol == "":
+    print ("The value is:")
+    print (randint(1,6))
+    rol = input("roll again?")
