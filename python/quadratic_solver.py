0a4aa180b484f2b1d2016b023cc7e536f5d36c4e
cmillar70 <55702245+cmillar70@users.noreply.github.com>
2021-09-21 09:23:46 -0400

    Add files via upload

---
+++ python/quadratic_solver.py
@@ -0,0 +1,24 @@
+from math import sqrt
+
+
+print("Quadratic Solver")
+print("enter the coefficients for ax^2 + bx + c = 0")
+print("enter the coefficient a")
+a = int(input())
+print("enter the coefficient b")
+b = int(input())
+print("enter the coefficient c")
+c = int(input())
+
+d = b ** 2 - 4 * a * c
+if(d > 0):
+    print("Root #1: ", 
+(-b + sqrt(b ** 2 - 4 * a * c))/ 2 * a
+)
+    print("Root #2: ", (-b - sqrt(b ** 2 - 4 * a * c))/ 2 * a)
+if(d == 0):
+    print("Root #1: ", 
+(-b + sqrt(b ** 2 - 4 * a * c))/ 2 * a
+)
+if(d < 0):
+    print("there are no real roots")
