9a699bcd317b375941b75ed3d80bf6f2ae568045
cmillar70 <55702245+cmillar70@users.noreply.github.com>
2021-09-21 10:08:29 -0400

    Add files via upload

---
+++ python/strings_and_loops.py
@@ -0,0 +1,11 @@
+#Calculator (ENGR4)
+#Written by Christian Millard
+#9.21.21
+
+print("type your text, then rpess enter: ")
+x = input()
+y = x.split(" ")
+for z in y:
+    for a in z:
+        print(a)
+    print("-")    
