4286a99b291070b82e050466492b390b97684096
cmillar70 <55702245+cmillar70@users.noreply.github.com>
2021-10-27 10:09:55 -0400

    Update strings_and_loops.py

--- python/strings_and_loops.py
+++ python/strings_and_loops.py
@@ -3,9 +3,9 @@
 #9.21.2021
 
 print("type your text, then press enter: ")
-x = input()
-y = x.split(" ")
-for z in y:
-    for a in z:
-        print(a)
-    print("-")    
+x = input() #the phrase you input
+y = x.split(" ") #splits the phrase into a list of words
+for z in y: #counts the number of words in the phrase
+    for a in z: #counts the number of letters in each word
+        print(a) # prints the next letter
+    print("-") #puts a dash between words
