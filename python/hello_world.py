e35601f38c21c5e8fcf4646cf437b8891ffd8c75
cmillar70 <55702245+cmillar70@users.noreply.github.com>
2021-10-27 09:48:33 -0400

    Update hello_world.py

--- python/hello_world.py
+++ python/hello_world.py
@@ -2,7 +2,7 @@
 #Written by Christian Millard
 #9.23.2021
 
-x = 1
-while(x<11):
-    print("Hello, World")
-    x = 1 + x
+x = 1 #starts the value at one
+while(x<11): #loops until the value reaches 11
+    print("Hello, World") #prints "hello world"
+    x = 1 + x #adds one to the value
